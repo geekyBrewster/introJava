7073ec4e978681aa2bddab43880cd0d793c1f0f9
Amanda Kirchner <geeky.brewster@gmail.com>
2017-10-20 14:28:33 -0500

    Added some info on expressions

---
+++ expressions.java
@@ -0,0 +1,24 @@
+public class Expressions {
+    public static void main(String[] args){
+//Keywords - there are 50 reserved words in Java
+//Example: int, double, if, for, else, etc.
+
+//Expression
+// Example: kilometers = (100 * 1.609344)
+
+//Statement
+
+//Variable declaration Statement
+// The data type and semicolor are part of the statement not expression
+  double kilometers = (100 * 1.609344);
+
+  int score = 100;  //score = 100 is an expression
+  if (score > 90) {   //score > 90 is an expression
+    System.out.println("You got the high score.");
+    score = 0;  //is an expression
+  }
+
+
+
+  }
+}
